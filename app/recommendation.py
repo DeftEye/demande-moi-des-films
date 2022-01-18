c3f426a67a08f7b5d30c5c59c593d4e1d95538b0
DeftEye <ego98a@gmail.com>
2022-01-18 12:20:18 +0100

    premiers changements

--- app/recommendation.py
+++ app/recommendation.py
@@ -56,32 +56,69 @@ class Recommendation:
 
     # Display the recommendation for a user
     def make_recommendation(self, user):
-        movie = choice(list(self.movies.values())).title
+        sortedUsers = sorted(self.compute_all_similarities(user), key=lambda l: l[1], reverse = True)
+        closest_user = self.test_users[sortedUsers[0][0]]
+        recommended_movies = ""
+        if hasattr(closest_user, 'good_ratings'):
+            for movie in closest_user.good_ratings:
+                recommended_movies += "," + movie.title
 
-        return "Vos recommandations : " + ", ".join([movie])
+            return "Vos recommandations : " + recommended_movies
+        else:
+            return "Il aime rien"
 
     # Compute the similarity between two users
     @staticmethod
     def get_similarity(user_a, user_b):
-        return 1
+        similarity = 0
+        nb_films_rated_by_both = 0
+
+        if hasattr(user_a, 'good_ratings'):
+            for likedMovie in user_a.good_ratings:
+                if hasattr(user_b, 'good_ratings') and likedMovie in user_b.good_ratings:
+                    similarity += 1 
+                    nb_films_rated_by_both += 1 
+                elif hasattr(user_b, 'bad_ratings') and likedMovie in user_b.bad_ratings:
+                    similarity -= 1
+                    nb_films_rated_by_both += 1
+        if hasattr(user_a, 'bad_ratings'):
+            for dislikedMovie in user_a.bad_ratings:
+                if hasattr(user_b, 'good_ratings') and dislikedMovie in user_b.good_ratings:
+                    similarity -= 1 
+                    nb_films_rated_by_both += 1 
+                elif hasattr(user_b, 'bad_ratings') and dislikedMovie in user_b.bad_ratings:
+                    similarity += 1
+                    nb_films_rated_by_both += 1    
+        if nb_films_rated_by_both == 0:
+            return 0
+        else :
+            return similarity/nb_films_rated_by_both
 
     # Compute the similarity between a user and all the users in the data set
     def compute_all_similarities(self, user):
-        return []
+        self.process_ratings_to_users()
+        all_similarities = []
+        for randomUser in self.test_users.keys():
+            all_similarities.append([randomUser, self.get_similarity(user, self.test_users[randomUser])])
+        return all_similarities
 
     @staticmethod
     def get_best_movies_from_users(users):
+
         return []
 
     @staticmethod
     def get_user_appreciated_movies(user):
+
         return []
 
     @staticmethod
     def get_user_norm(user):
+
         return 1
 
     # Return a vector with the normalised ratings of a user
     @staticmethod
     def get_normalised_cluster_notations(user):
         return []
+
